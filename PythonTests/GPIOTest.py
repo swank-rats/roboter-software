fc1cd1dff32c9ced3227f0c8b86b2b32e38e6f1f
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2014-10-23 16:26:52 +0200

    Read input

--- PythonTests/GPIOTest.py
+++ PythonTests/GPIOTest.py
@@ -2,13 +2,15 @@ __author__ = 'Johannes'
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-P = "P8_11"
+P = "USR3"
 
 GPIO.setup(P, GPIO.OUT)
 for i in xrange(100):
         if i % 2 == 0:
                 GPIO.output(P, GPIO.HIGH)
+                print "HIGH"
         else:
                 GPIO.output(P, GPIO.LOW)
+                print "LOW"
         time.sleep(0.5)
-GPIO.cleanup() 
+GPIO.cleanup()
