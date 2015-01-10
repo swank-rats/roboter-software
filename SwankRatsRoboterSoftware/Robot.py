704a4b170566a5b813238ceff121441691bc33b9
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2015-01-10 11:59:14 +0100

    asdf

--- SwankRatsRoboterSoftware/Robot.py
+++ SwankRatsRoboterSoftware/Robot.py
@@ -5,43 +5,53 @@ import time
 
 
 class Robot:
+
     def setLeftMotor(self, percent):
+        print('leftMotor ' + str(percent))
+        DMCC.setMotor(0, 1, int(percent * 100))
+
+    def setRightMotor(self, percent):
+        print('rightMotor ' + str(percent))
+        DMCC.setMotor(0, 2, int(-percent * 100))
+
+
+    def setLeftMotor2(self, percent):
         actuel = 0
         if percent == 0:
             print('leftMotor ' + str(percent))
-            DMCC.setMotor(0, 1, int(percent * 100 * 0.60))
+            DMCC.setMotor(0, 1, int(percent * 100 ))
         if percent > 0:
             actuel = 10
             while actuel <= percent:
                 print('leftMotor ' + str(actuel))
-                DMCC.setMotor(0, 1, int(actuel * 100 * 0.60))
+                DMCC.setMotor(0, 1, int(actuel * 100))
                 actuel += 5
                 time.sleep(0.30)
         if percent < 0:
             actuel = -10
             while actuel >= percent:
                 print('leftMotor ' + str(actuel))
-                DMCC.setMotor(0, 1, int(actuel * 100 * 0.60))
+                DMCC.setMotor(0, 1, int(actuel * 100))
                 actuel -= 5
                 time.sleep(0.300)
 
-    def setRightMotor(self, percent):
+    def setRightMotor2(self, percent):
         actuel = 0
         if percent == 0:
             print('rightMotor ' + str(percent))
-            DMCC.setMotor(0, 2, int(percent * 100 * 0.60))
+            DMCC.setMotor(0, 2, int(percent * 100))
         if percent > 0:
             actuel = 10
             while actuel <= percent:
                 print('rightMotor ' + str(actuel))
-                DMCC.setMotor(0, 2, int(actuel * 100 * 0.60))
+                DMCC.setMotor(0, 2, int(actuel * 100))
                 actuel += 5
                 time.sleep(0.30)
         if percent < 0:
             actuel = -10
             while actuel >= percent:
                 print('rightMotor ' + str(actuel))
-                DMCC.setMotor(0, 2, int(actuel * 100 * 0.60))
+                DMCC.setMotor(0, 2, int(actuel * 100))
                 actuel -= 5
                 time.sleep(0.30)
 
