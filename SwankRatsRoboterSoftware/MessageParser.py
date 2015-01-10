e2f1cc35093d3dc7c877f4c07a7e3518e2bab400
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2015-01-10 11:38:48 +0100

    asdf

--- SwankRatsRoboterSoftware/MessageParser.py
+++ SwankRatsRoboterSoftware/MessageParser.py
@@ -14,21 +14,21 @@ class MessageParser:
         self.currentState = StateClasses.Stop()
 
     def parse(self, jsonString):
-        try:
-            data = json.loads(jsonString)
-            if( data["to"] == "robot"):
-                if(data["params"]["started"] == True):
-                    self.currentState = self.currentState.press(data["cmd"])
-                if(data["params"]["started"] == False):
-                    self.currentState = self.currentState.release(data["cmd"])
+        #try:
+        data = json.loads(jsonString)
+        if( data["to"] == "robot"):
+            if(data["params"]["started"] == True):
+                self.currentState = self.currentState.press(data["cmd"])
+            if(data["params"]["started"] == False):
+                self.currentState = self.currentState.release(data["cmd"])
 
-                self.robot.setLeftMotor(self.currentState.getLeft())
-                self.robot.setRightMotor(self.currentState.getRight())
+            self.robot.setLeftMotor(self.currentState.getLeft())
+            self.robot.setRightMotor(self.currentState.getRight())
 
-            if(data["to"] == "print"):
-                 json.dumps(data)
+        if(data["to"] == "print"):
+             json.dumps(data)
 
-            json.dumps(data)
-        except :
-            e = sys.exc_info()[0]
-            print e
+        json.dumps(data)
+        #except :
+         #   e = sys.exc_info()[0]
+          #  print e
