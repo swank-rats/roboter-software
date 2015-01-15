33de04f052216c30aeafc91327abaf373f184dc3
Wachter Johannes <johannes.wachter@massiveart.at>
2015-01-15 14:35:57 +0100

    clean up code

--- SwankRatsRoboterSoftware/Robot.py
+++ SwankRatsRoboterSoftware/Robot.py
@@ -1,64 +1,61 @@
 __author__ = 'Johannes'
-# import Adafruit_BBIO.GPIO as GPIO
 
 import DMCC
 import time
 import RobotConfig
-from thread import start_new_thread
 import threading
 
 class Robot:
     def __init__(self):
-        self.rightMax = RobotConfig.Config.getfloat('robot', 'rightMax')
-        self.leftMax = RobotConfig.Config.getfloat('robot', 'leftMax')
-        self.rightMax = self.rightMax / 100.0
-        self.leftMax = self.leftMax / 100.0
+        rm = RobotConfig.Config.getfloat('robot', 'rightMax')
+        lm = RobotConfig.Config.getfloat('robot', 'leftMax')
+        self.rightMax = rm / 100.0
+        self.leftMax = lm / 100.0
         self.right = 0
         self.left = 0
 
     def setLeftMotor(self, percent):
-        #start_new_thread(setMotor, (self.left,percent,5,"left",self.leftMax,0.02))
-        t = threading.Thread(target=setMotor, args=(self.right,percent,5,"left",self.rightMax,0.02))
+        t = threading.Thread(target=setMotor, args=(self.right, percent, 5, "left", self.rightMax, 0.02))
         t.start()
         self.left = percent
 
     def setRightMotor(self, percent):
-        t = threading.Thread(target=setMotor, args=(self.right,percent,5,"right",self.rightMax,0.02))
+        t = threading.Thread(target=setMotor, args=(self.right, percent, 5, "right", self.rightMax, 0.02))
         t.start()
-        #start_new_thread(setMotor, (self.right,percent,5,"right",self.rightMax,0.02))
         self.right = percent
 
+
 def setMotor(start, stop, step, motor, max, pause):
-    if(start < stop):
+    if (start < stop):
         up(start, stop, step, motor, max, pause)
-    if(start > stop):
+    if (start > stop):
         down(start, stop, step, motor, max, pause)
 
+
 def up(start, stop, step, motor, max, pause):
-    print "up"
+    print "up " + str(start) + " " + str(stop)
     while start <= stop:
+        print(start)
+        set(motor, start, max)
         start += step
         time.sleep(pause)
-        print(start)
-#        try:
-#            if motor == "left":
-#                DMCC.setMotor(0, 1, int(start * 100 * max))
-#            if motor == "right":
-#                DMCC.setMotor(0, 2, int(start * 100 * max))
-#        except:
-#            print "Error setting " + motor + " motor, trying again"
+
 
 def down(start, stop, step, motor, max, pause):
-    print "down "+str(start)+ " "+str(stop)
+    print "down " + str(start) + " " + str(stop)
     while start >= stop:
+        print(start)
+        set(motor, start, max)
         start -= step
         time.sleep(pause)
-        print(start)
-#        try:
-#            if motor == "left":
-#                DMCC.setMotor(0, 1, int(start * 100 * max))
-#            if motor == "right":
-#                DMCC.setMotor(0, 2, int(start * 100 * max))
-#        except:
-#            print "Error setting " + motor + " motor, trying again"
 
+
+def set(motor, value, max):
+    try:
+        value = int(value * 100 * max)
+        if motor == "left":
+            DMCC.setMotor(0, 1, value)
+        if motor == "right":
+            DMCC.setMotor(0, 2, value)
+    except:
+        print "Error setting " + motor + " motor, trying again"
