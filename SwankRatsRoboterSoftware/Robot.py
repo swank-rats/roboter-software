757b6f903b8b10a231459638fb188f0cc3775236
Johannes Schwendinger <johannes.schwendinger@hotmail.com>
2015-01-15 13:56:32 +0100

    asdfsdf

--- SwankRatsRoboterSoftware/Robot.py
+++ SwankRatsRoboterSoftware/Robot.py
@@ -7,28 +7,42 @@ import RobotConfig
 from thread import start_new_thread
 
 class Robot:
-
     def __init__(self):
         self.rightMax = RobotConfig.Config.getfloat('robot', 'rightMax')
         self.leftMax = RobotConfig.Config.getfloat('robot', 'leftMax')
         self.rightMax = self.rightMax / 100.0
         self.leftMax = self.leftMax / 100.0
+        self.right = 0
+        self.left = 0
 
     def setLeftMotor(self, percent):
-        if(percent == 0):
-            DMCC.setMotor(0, 1, 0)
-        else:
-            start_new_thread(setMotor, (10,percent,5,"left",self.leftMax,0.01))
+        start_new_thread(setMotor, (self.left,percent,5,"left",self.leftMax,0.02))
 
     def setRightMotor(self, percent):
-        if(percent == 0):
-            DMCC.setMotor(0, 2, 0)
-        else:
-            start_new_thread(setMotor, (10,percent,5,"right",self.rightMax,0.01))
+        start_new_thread(setMotor, (self.right,percent,5,"right",self.rightMax,0.02))
 
 def setMotor(start, stop, step, motor, max, pause):
+    if(start > stop):
+        up(start, stop, step, motor, max, pause)
+    if(start < stop):
+        up(start, stop, step, motor, max, pause)
+
+def up(start, stop, step, motor, max, pause):
     while start <= stop:
         start += step
+        time.sleep(pause)
+        print(start)
+        try:
+            if motor == "left":
+                DMCC.setMotor(0, 1, int(start * 100 * max))
+            if motor == "right":
+                DMCC.setMotor(0, 2, int(start * 100 * max))
+        except:
+            print "Error setting " + motor + " motor, trying again"
+
+def down(start, stop, step, motor, max, pause):
+    while start >= stop:
+        start -= step
         time.sleep(pause)
         print(start)
         try:
